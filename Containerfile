9d4614334c60f8a654a68783efd17ad8e23e6fff
mikelo <michele.orlandi@gmail.com>
2025-02-24 13:23:10 +0100

    testing tests

--- Containerfile
+++ Containerfile
@@ -7,15 +7,15 @@ ENV LICENSE=accept
 ENV DATA=/tmp/data
 ENV SKIP=""
 # VOLUME [ "/tmp/data" ]
-COPY --from=git --chown=aceuser:mqbrkrs /tmp/ace-source /tmp/coffee
+COPY --from=git --chown=aceuser:mqbrkrs /tmp/ace-source/ /tmp/
 # RUN . /opt/ibm/ace-13/server/bin/mqsiprofile && jar xvf gradle.zip > /dev/null && chmod +x gradle-8.13-milestone-3/bin/gradle && \
 # ibmint package --input-path coffee --output-bar-file coffee.bar --overrides-file coffee/overrides.properties && \
 # mqsicreateworkdir /tmp/work-dir && ibmint deploy --input-bar-file coffee.bar --output-work-directory /tmp/work-dir
-WORKDIR /tmp/coffee
-# ADD --chown=aceuser:mqbrkrs https://services.gradle.org/distributions/gradle-8.13-milestone-3-bin.zip gradle.zip
-ENV MQSI_JARPATH=$MQSI_JARPATH:/tmp/coffee/ACME_CoffeeRoasters_Java
-RUN . /opt/ibm/ace-13/server/bin/mqsiprofile && ibmint package --input-path . --output-bar-file coffee.bar --project ACME_CoffeeRoasters_Application --project ACME_CoffeeRoasters_Java --project ACME_CoffeeRoasters_UnitTest --project ACME_CoffeeRoasters_ComponentTest && mqsicreateworkdir /tmp/work-dir && ibmint deploy --input-bar-file coffee.bar --output-work-directory /tmp/work-dir
+WORKDIR /tmp/
+ADD --chown=aceuser:mqbrkrs https://services.gradle.org/distributions/gradle-8.13-milestone-3-bin.zip /tmp/gradle.zip
+ENV MQSI_JARPATH=$MQSI_JARPATH:/tmp/ACME_CoffeeRoasters_Java
+RUN . /opt/ibm/ace-13/server/bin/mqsiprofile && jar xvf gradle.zip > /dev/null && chmod +x gradle-8.13-milestone-3/bin/gradle && ibmint package --input-path . --output-bar-file coffee.bar --project ACME_CoffeeRoasters_Application --project ACME_CoffeeRoasters_Java && mqsicreateworkdir /tmp/work-dir && ibmint deploy --input-bar-file coffee.bar --output-work-directory /tmp/work-dir
 ADD entrypoint.sh .
 EXPOSE 7600
 EXPOSE 7800
-ENTRYPOINT [ "/tmp/coffee/entrypoint.sh" ]
+ENTRYPOINT [ "/tmp/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,29 +2,22 @@
 source /opt/ibm/ace-13/server/bin/mqsiprofile
 
 runtests () {
-    # ibmint generate tests --recorded-messages $DATA --output-test-project /tmp/test-project --java-class com.ibm.ClassName 
-    # cd /tmp/coffee/ACME_CoffeeRoasters_UnitTest && /tmp/coffee/gradle-8.13-milestone-3/bin/gradle
-    # cp -vr /tmp/coffee/ACME_CoffeeRoasters_UnitTest /tmp/work-dir/run 
-    # ibmint deploy --input-path /tmp/coffee/ 
-    IntegrationServer --work-dir /tmp/work-dir --test-project ACME_CoffeeRoasters_UnitTest --start-msgflows false
+    rm -fr /tmp/work-dir/run /tmp/test-project 2>/dev/null
+    ibmint generate tests --recorded-messages /tmp/$1/src/main/resources --output-test-project test-project --java-class com.ibm.dev.$1 
+    cd /tmp/test-project && /tmp/gradle-8.13-milestone-3/bin/gradle
+    cp -vr /tmp/test-project /tmp/work-dir/run 
+    IntegrationServer --work-dir /tmp/work-dir --test-project test-project --start-msgflows false --default-application-name test-project
     EC=$?
     if [[ $EC != 0 ]]; then
-        echo "ACME_CoffeeRoasters_UnitTest failed with exit code $EC"
+        echo "$1 failed with exit code $EC"
         exit $EC
-    fi
-    # rm -fr /tmp/work-dir/run && cd /tmp/coffee/ACME_CoffeeRoasters_ComponentTest && /tmp/coffee/gradle-8.13-milestone-3/bin/gradle
-    # cp -vr /tmp/coffee/ACME_CoffeeRoasters_ComponentTest /tmp/work-dir/run
-    IntegrationServer --work-dir /tmp/work-dir --test-project ACME_CoffeeRoasters_ComponentTest --start-msgflows false
-    EC=$?
-    if [[ $EC != 0 ]]; then
-        echo "ACME_CoffeeRoasters_ComponentTest failed with exit code $EC"
-        # exit $EC
     fi    
 }
 
 if [[ -z $SKIP ]]; then
     echo "SKIP variable not set! Running tests!"
-    runtests
+    runtests ACME_CoffeeRoasters_UnitTest
+    runtests ACME_CoffeeRoasters_ComponentTest
 fi
 
 IntegrationServer --work-dir /tmp/work-dir --start-msgflows true
