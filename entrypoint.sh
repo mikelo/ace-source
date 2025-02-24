8a316a50ebaf9b79062150f7435dfdfe611a9f67
mikelo <michele.orlandi@gmail.com>
2025-02-24 10:19:53 +0100

    renamed Transform node

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ runtests () {
     # cd /tmp/coffee/ACME_CoffeeRoasters_UnitTest && /tmp/coffee/gradle-8.13-milestone-3/bin/gradle
     # cp -vr /tmp/coffee/ACME_CoffeeRoasters_UnitTest /tmp/work-dir/run 
     # ibmint deploy --input-path /tmp/coffee/ 
-    ibmint create node Transform && ibmint start node Transform
     IntegrationServer --work-dir /tmp/work-dir --test-project ACME_CoffeeRoasters_UnitTest --start-msgflows false
     EC=$?
     if [[ $EC != 0 ]]; then
@@ -20,8 +19,7 @@ runtests () {
     if [[ $EC != 0 ]]; then
         echo "ACME_CoffeeRoasters_ComponentTest failed with exit code $EC"
         # exit $EC
-    fi
-    ibmint stop node Transform
+    fi    
 }
 
 if [[ -z $SKIP ]]; then
